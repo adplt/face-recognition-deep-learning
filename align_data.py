c033c878902e5d0d46adad0a21d5ef5b425e42b2
adplt <atriadplt@gmail.com>
2018-10-09 23:09:53 +0700

    add align function for youtube face dataset

--- align_data.py
+++ align_data.py
@@ -1,9 +1,8 @@
-from imutils.face_utils import FaceAligner, rect_to_bb
-from PIL import Image
-import imutils, dlib, cv2, os, inspect, glob, shutil, numpy, png
+from imutils.face_utils import FaceAligner
+import dlib, cv2, os, inspect
 
 
-def align_face(args):
+def align_face_lfw(args):
     curr_directory = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     input_dir = os.path.join(curr_directory, 'input_dir')
     out_dir = os.path.join(curr_directory, 'out_dir')
@@ -12,13 +11,10 @@ def align_face(args):
     if os.path.exists(out_dir) and len(os.listdir(out_dir)) <= 1 and args.shapePredictor is not None:
         print 'Face Aligning ...'
 
-        # initialize dlib's face detector (HOG-based) and then create
-        # the facial landmark predictor and the face aligner
         detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-        fa = FaceAligner(predictor, desiredFaceWidth=64, desiredFaceHeight=64)
+        predictor = dlib.shape_predictor(os.path.join(curr_directory, args.shapePredictor))
+        fa = FaceAligner(predictor, desiredFaceWidth=32, desiredFaceHeight=32)
 
-        # load the input image, resize it, and convert it to gray scale
         i = 0
         while i < len(list_label):
             j = 0
@@ -30,26 +26,14 @@ def align_face(args):
                 image = cv2.imread(os.path.join(input_dir, label + '/' + input_label_dir[j]))
 
                 # Bicubic Interpolation: extension dari cubic interpolation, membuat permukaan gambar jadi lebih lembut
-                image = cv2.resize(image, (64,64), fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
                 # tuple dapat diisi dengan None (size'a bakal ngikutin yg default dari OpenCV)
+                image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
 
-                # detect faces in the grayscale
-                gray = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Changing Color Space - COLOR_BGR2GRAY
+                gray = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                 rects = detector(gray, 3)
 
-                # loop over the face detections
                 for rect in rects:
-                    print rect
-                    # extract the ROI of the *original* face, then align the face
-                    # using facial landmarks
-                    # (x, y, w, h) = rect_to_bb(rect)
-                    # face_ori = imutils.resize(image[y:y + h, x:x + w], width=256)
-                    print face_aligned
                     face_aligned = fa.align(image, gray, rect)
-
-                    # display the output images
-                    # cv2.imshow('Aligned', face_aligned)
-                    # cv2.waitKey(0)
                 if not os.path.exists(output_label_dir):
                     os.makedirs(output_label_dir)
                     cv2.imwrite(os.path.join(output_label_dir, input_label_dir[j]), face_aligned)
@@ -57,6 +41,120 @@ def align_face(args):
             i += 1
         print 'Successfully face aligned and copy new dataset to to output_dir'
     elif args.shapePredictor is None:
-        print 'you don\'t have shapePredictor so you cannot continuing to alignment face'
+        print 'You don\'t have shapePredictor so you cannot continuing to alignment face'
+    else:
+        print ''
+
+
+def align_face_youtube_face(args):
+    curr_directory = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    input_dir = os.path.join(curr_directory, 'input_dir')
+    out_dir = os.path.join(curr_directory, 'out_dir')
+    list_label = os.listdir(input_dir)
+
+    if os.path.exists(out_dir) and len(os.listdir(out_dir)) <= 1 and args.shapePredictor is not None:
+        print 'Face Aligning ...'
+
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(os.path.join(curr_directory, args.shapePredictor))
+        fa = FaceAligner(predictor, desiredFaceWidth=32, desiredFaceHeight=32)
+
+        i = 0
+        while i < len(list_label):
+            j = 0
+            label_path_in = os.path.join(input_dir, list_label[i])
+            label_path_out = os.path.join(out_dir, list_label[i])
+            frame_index_list = os.listdir(label_path_in)
+
+            face_aligned = None
+
+            while j < len(frame_index_list):
+                k = 0
+                frame_index_path = os.path.join(label_path_in, frame_index_list[j])
+                input_label_dir = os.listdir(frame_index_path)
+
+                # print 'label_path_in: ' + frame_index_list[j]
+                # print 'frame_index_path: ' + frame_index_path
+                # print 'input_label_dir: ' + str(len(input_label_dir))
+                # print 'j: ' + str(j)
+                # print '\n\n\n\n'
+
+                while k < len(input_label_dir):
+                    # print 'k: ' + str(k)
+                    # print 'frame_index_path: ' + frame_index_path
+                    # print 'input_label_dir[k]: ' + input_label_dir[k]
+                    # print 'os.path.join(frame_index_path, input_label_dir[k]): ' +\
+                    #       os.path.join(frame_index_path, input_label_dir[k])
+                    # print '\n'
+
+                    image = cv2.imread(os.path.join(frame_index_path, input_label_dir[k]))
+
+                    # Bicubic Interpolation:
+                    # extension dari cubic interpolation, membuat permukaan gambar jadi lebih lembut
+                    # tuple dapat diisi dengan None (size'a bakal ngikutin yg default dari OpenCV)
+                    image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+
+                    gray = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                    rects = detector(gray, 3)
+
+                    for rect in rects:
+                        face_aligned = fa.align(image, gray, rect)
+                    if not os.path.exists(label_path_out):
+                        os.makedirs(label_path_out)
+                        cv2.imwrite(os.path.join(label_path_out, str(input_label_dir[j])), face_aligned)
+                    else:
+                        print 'label_path_out: ' + label_path_out
+                        print 'input_label_dir[j]: ' + input_label_dir[k]
+                        cv2.imwrite(os.path.join(label_path_out, input_label_dir[k]), face_aligned)
+                    k += 1
+                j += 1
+            i += 1
+        print 'Successfully face aligned and copy new dataset to to output_dir'
+    elif args.shapePredictor is None:
+        print 'You don\'t have shapePredictor so you cannot continuing to alignment face'
+    else:
+        print ''
+
+
+def align_face_feret_color(args):
+    curr_directory = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    input_dir = os.path.join(curr_directory, 'input_dir')
+    out_dir = os.path.join(curr_directory, 'out_dir')
+    list_label = os.listdir(input_dir)
+
+    if os.path.exists(out_dir) and len(os.listdir(out_dir)) <= 1 and args.shapePredictor is not None:
+        print 'Face Aligning ...'
+
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(os.path.join(curr_directory, args.shapePredictor))
+        fa = FaceAligner(predictor, desiredFaceWidth=32, desiredFaceHeight=32)
+
+        i = 0
+        while i < len(list_label):
+            j = 0
+            label = list_label[i]
+            input_label_dir = os.listdir(os.path.join(input_dir, label))
+            output_label_dir = os.path.join(out_dir, label)
+            face_aligned = None
+            while j < len(input_label_dir):
+                image = cv2.imread(os.path.join(input_dir, label + '/' + input_label_dir[j]))
+
+                # Bicubic Interpolation: extension dari cubic interpolation, membuat permukaan gambar jadi lebih lembut
+                # tuple dapat diisi dengan None (size'a bakal ngikutin yg default dari OpenCV)
+                image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+
+                gray = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                rects = detector(gray, 3)
+
+                for rect in rects:
+                    face_aligned = fa.align(image, gray, rect)
+                if not os.path.exists(output_label_dir):
+                    os.makedirs(output_label_dir)
+                    cv2.imwrite(os.path.join(output_label_dir, input_label_dir[j]), face_aligned)
+                j += 1
+            i += 1
+        print 'Successfully face aligned and copy new dataset to to output_dir'
+    elif args.shapePredictor is None:
+        print 'You don\'t have shapePredictor so you cannot continuing to alignment face'
     else:
         print ''
--- main.py
+++ main.py
@@ -1,46 +1,9 @@
-import create_dir
 import align_data
-from six.moves import urllib
-import sys
-import os
-import tarfile
 import argparse
-from tqdm import tqdm
-import requests
-import shutil
-
-
-TRAINING_DATA = 'http://vis-www.cs.umass.edu/lfw/lfw.tgz'
-# TRAINING_DATA = 'http://www.security-camera-warehouse.com/images/lens-distance/50ft/3MP-12mm.png'
-
-
-file_name = TRAINING_DATA.split('/')[-1]
-folder_name = file_name[:file_name.index('.')]
-
-chunk_size = 1024
 
 
 def main(argv):
-    create_dir
-    r = requests.get(TRAINING_DATA, stream=True)
-    if r.headers.get('content-length') is not None:
-        size = int(r.headers.get('content-length'))
-    else:
-        size = 0
-    if not os.path.exists('input_dir') and not os.path.isdir('input_dir') and not os.path.exists('lfw.tgz'):
-        r = requests.get(TRAINING_DATA, stream=True)
-        with open(file_name, 'wb') as f:
-            for data in tqdm(iterable=r.iter_content(chunk_size=chunk_size), total=size / chunk_size, unit='KB'):f.write(data)
-        print 'Successfully downloaded ' + file_name + ' with ' + str(size / 1000000) + ' MB'
-    if os.path.exists('input_dir') and os.path.exists('lfw.tgz'):
-        print 'Extracting ' + file_name + ' ...'
-        tarfile.open(file_name).extractall()
-        os.rename(folder_name, 'input_dir')
-        os.remove(file_name)
-        print 'Successfully extracted ' + file_name + ' to input_dir and remove ' + file_name
-    else:
-        print 'All the input data have been complete'
-    align_data.align_face(argv)
+    align_data.align_face_youtube_face(argv)
 
 
 if __name__ == '__main__':
@@ -50,6 +13,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
 
-
 # Example:
-# python main.py --shape-predictor shape_predictor_68_face_landmarks.dat --image input_dir/Aaron_Eckhart/Aaron_Eckhart_0001.jpg
+# python main.py --shape-predictor shape_predictor_68_face_landmarks.dat
